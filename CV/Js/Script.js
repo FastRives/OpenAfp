008f8a25de13f72f9098e6eaf9ba0b8012247101
FastRives <tom.frassineti@gmail.com>
2020-10-16 12:08:52 +0200

    Update CV + Chapitre UML
    Update du CV à l'aide des cours suivis avec le designer du web.
    Ajout de la partie UML/ des différents diagrammes et de la maquette (Adobe XD) du cas médiathèque.

--- CV/Js/Script.js
+++ CV/Js/Script.js
@@ -1,6 +1,31 @@
     const containerMenu = document.querySelector('.container-menu');
     const btnMenu = document.querySelector('.btn-menu');
+    const allRonds = document.querySelectorAll('.rond');
+    const allBoxes = document.querySelectorAll('.box');
+    const controller = new ScrollMagic.Controller()
 
     btnMenu.addEventListener('click', () => {
         containerMenu.classList.toggle('active');
-    })  
+    });
+
+    allBoxes.forEach(box => {
+
+        for(i = 0; i < allRonds.length; i++){
+
+            if(allRonds[i].getAttribute('data-anim') === box.getAttribute('data-anim')){
+
+                let tween = gsap.from(box, {y: -50, opacity: 0, duration: 0.5})
+
+                let scene = new ScrollMagic.Scene({
+                    triggerElement: allRonds[i],
+                    reverse: false
+                })
+                .setTween(tween)
+                // .addIndicators()
+                .addTo(controller)
+
+            }
+
+        }
+
+    })
