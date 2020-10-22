5023eff5886190b78e163a55818d3ad95c565b98
FastRives <tom.frassineti@gmail.com>
2020-10-22 16:55:27 +0200

    Last Update CV
    Ajout du module de scroll en js

--- CV/Js/Script.js
+++ CV/Js/Script.js
@@ -1,8 +1,12 @@
     const containerMenu = document.querySelector('.container-menu');
     const btnMenu = document.querySelector('.btn-menu');
     const allRonds = document.querySelectorAll('.rond');
-    const allBoxes = document.querySelectorAll('.box');
-    const controller = new ScrollMagic.Controller()
+    const allBoxes = document.querySelectorAll('.boxmagic');
+    const controller = new ScrollMagic.Controller();
+    const Menu2 = document.getElementById('Menu2');
+    const Menu3 = document.getElementById('Menu3');
+    const Menu4 = document.getElementById('Menu4');
+    const Menu5 = document.getElementById('Menu5');
 
     btnMenu.addEventListener('click', () => {
         containerMenu.classList.toggle('active');
@@ -21,11 +25,50 @@
                     reverse: false
                 })
                 .setTween(tween)
-                // .addIndicators()
+                //.addIndicators()
                 .addTo(controller)
-
             }
-
         }
-
     })
+
+function smoothScroll(target, duration){
+    var target = document.querySelector(target);
+    console.log(target);
+    var targetPosition = target.getBoundingClientRect().top;
+    var startPosition = window.pageYOffset;
+    var distance = targetPosition;
+    var startTime = null;
+    console.log("StartPosition : "+startPosition);
+    console.log("targetPosition : "+targetPosition);
+    console.log("Distance : "+distance);    
+
+    function animation(currentTime){
+        if(startTime === null) startTime = currentTime;
+        var timeElapsed = currentTime - startTime;
+        var run = ease(timeElapsed, startPosition, distance, duration);
+        window.scrollTo(0, run);
+        if(timeElapsed < duration) requestAnimationFrame(animation);
+    }
+
+    function ease(t,b,c,d){
+        t /= d / 2;
+        if (t < 1) return c / 2 * t * t +b;
+        t--;
+        return -c / 2 * (t *(t - 2) - 1) +b;
+    }
+
+    requestAnimationFrame(animation);
+}
+
+Menu2.addEventListener('click', function() {
+    smoothScroll('#slide1', 1000);
+})
+Menu3.addEventListener('click', function() {
+    smoothScroll('#slide3', 1000);
+})
+Menu4.addEventListener('click', function() {
+    smoothScroll('#slide4', 1000);
+})
+Menu5.addEventListener('click', function() {
+    smoothScroll('#slide5', 1000);
+})
